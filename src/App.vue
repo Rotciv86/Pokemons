27ea7561a5d49360710a8e8676fb120953294a93
Víctor <Victor@MacBook-Air-de-Victor.local>
2024-06-18 10:33:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-emerald-800 font-bold underline">
+      {{ title }} 
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title : "Hello project 1 of the module 2"
+    }
+  }
+}
+</script>
