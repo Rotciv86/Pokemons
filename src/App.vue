ebd637a474928729b7f3150ad29c3c23f0f02048
Víctor <Victor@MacBook-Air-de-Victor.local>
2024-06-20 17:51:31 +0200

    Add initial functionality of the main fetch

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@
     <h1 class="text-3xl text-emerald-800 font-bold underline">
       {{ title }} 
     </h1>
+    <button @click="nextPage">NEXT</button>
+
+    <button @click="getUrls" type="button">Pokemons</button>
+    <!-- <p>{{ data }}</p> -->
+    <h2>Resultados</h2>
+    <!-- <p>{{pokemons}}</p> -->
+    <ul>
+      <li v-for="pokemon in pokemons" :key="pokemon">
+        <div>
+           <img :src="pokemon.sprites.other.dream_world.front_default" :alt="pokemon.name">
+           <h3>{{pokemon.name}}</h3>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -10,7 +24,39 @@ export default {
   name: "App",
   data() {
     return {
-      title : "Hello project 1 of the module 2"
+      title : "Pokemon",
+      data : "",
+      pokemons : []
+    }
+  },
+  mounted() {
+    
+    const data =  fetch("https://pokeapi.co/api/v2/pokemon/").then(response => response.json()).then(data => this.data = data).catch(error => console.error(error))
+
+    const results = data.results
+
+for (let pokemon of results){
+  fetch(`${pokemon.url}`).then(response => response.json()).then(data => this.pokemons.push(data)).catch(error => console.error(error))
+}
+  },
+  methods : {
+    getUrls() {
+      const results = this.data.results
+
+      for (let pokemon of results){
+        fetch(`${pokemon.url}`).then(response => response.json()).then(data => this.pokemons.push(data)).catch(error => console.error(error))
+      }
+
+    },
+    nextPage() {
+      fetch(`${this.data.next}`).then(response => response.json()).then(data => this.data = data).catch(error => console.error(error))
+      this.pokemons = []
+      this.getUrls()
+    }
+  },
+  watch: {
+    pokemons(newPokemons, oldPokemons) {
+      this.pokemons = newPokemons
     }
   }
 }
